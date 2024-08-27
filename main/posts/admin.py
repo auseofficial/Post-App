3699f9a1621b5766ea6f6e3ba046f37fbe787b13
Akib Us Suny Eshan <eshan.r029@gmail.com>
2024-08-27 11:32:55 +0600

    add user authentication

--- main/posts/admin.py
+++ main/posts/admin.py
@@ -3,15 +3,16 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('post_title', 'post_body')
+    list_per_page = 2  
 
     def post_title(self, obj):
         return obj.title
     post_title.short_description = 'Title'
-    post_title.admin_order_field = None  # Disable sorting for title
+    post_title.admin_order_field = None  
 
     def post_body(self, obj):
         return obj.body
     post_body.short_description = 'Body'
-    post_body.admin_order_field = None  # Disable sorting for body
+    post_body.admin_order_field = None  
 
 admin.site.register(Post, PostAdmin)
