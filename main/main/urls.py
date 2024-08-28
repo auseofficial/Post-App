cd29ab94ceb86d32f0777c4f229779ff66471131
Akib Us Suny Eshan <eshan.r029@gmail.com>
2024-08-28 12:43:23 +0600

    add user authentication

--- main/main/urls.py
+++ main/main/urls.py
@@ -14,4 +14,3 @@ urlpatterns = [
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
-
